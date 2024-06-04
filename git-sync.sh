771731f201849badb66d2e03c9b097ba843268b4
myml <wurongjie@deepin.org>
2024-06-04 17:15:24 +0800

    feat: 支持同步分支匹配

--- git-sync.sh
+++ git-sync.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
-
+set -x
 origin=$1
 target=$2
-force=$3
 
 rm -rf .cache .git
 
@@ -11,10 +10,15 @@ git clone --bare "$target" .cache || true
 git clone --reference-if-able .cache --bare "$origin" .git
 git remote add upstream "$target"
 
-if [ "$force" ]; then
-    git push --force --all --prune upstream
-    git push --force --tags upstream
-else
-    git push --all --prune upstream
-    git push --tags upstream
+force=
+if [ "$SYNC_FORCE_PUSH" ]; then
+  force="--force"
 fi
+
+if [ "$SYNC_BRANCH_PATTERN" ]; then
+    patterns=$(echo "$SYNC_BRANCH_PATTERN" | tr ';' ' ')
+    git branch --format '%(refname)' --list $patterns | xargs -i git push $force --prune upstream {}
+else
+    git push $force --all --prune upstream
+fi
+git push $force --tags upstream
