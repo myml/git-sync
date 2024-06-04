6de6512fc4ee31b2d7afb46e211ab409c30b4777
myml <wurongjie@deepin.org>
2024-06-04 15:33:06 +0800

    fix: run failed

--- git-sync.sh
+++ git-sync.sh
@@ -5,8 +5,16 @@ origin=$1
 target=$2
 force=$3
 
+rm -rf .cache .git
+
 git clone --bare "$target" .cache || true
 git clone --reference-if-able .cache --bare "$origin" .git
 git remote add upstream "$target"
-git push "$force" --all --prune upstream
-git push "$force" upstream --tags
+
+if [ $force ]; then
+    git push --force --all --prune
+    git push --force upstream --tags
+else
+    git push --all --prune
+    git push upstream --tags
+fi
